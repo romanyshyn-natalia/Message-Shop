1701a39286799db4781536b15761b878eb74ec87
yxurrbzallkd <60686597+yxurrbzallkd@users.noreply.github.com>
2021-05-06 12:34:52 +0300

    Update requests.sql

--- requests.sql
+++ requests.sql
@@ -41,7 +41,13 @@ SELECT customer_id FROM
 Access_token WHERE author_id=9 AND Access_token.status=true
 OR Access_token.given between '2019-08-11'::date and '2019-09-07';
 
-
+-- 7.	для покупця С знайти усiх авторiв, яким вiн надав доступ до хоча б одного облiкового запису у соцiальнiй мережi, а потiм позбавив його цього доступу;
+SELECT Author.name as authors
+FROM Access_token
+INNER JOIN (SELECT customer_id,name FROM Customer WHERE name='Dmytro') AS C
+ON Access_token.customer_id=C.customer_id
+INNER JOIN Author ON Author.author_id=Access_token.author_id
+WHERE Access_token.status=false;
 
 -- 8.	знайти усi спiльнi подiї для автора A та покупця С за вказаний перiод (з дати F по дату T);
 -- Події
@@ -52,51 +58,60 @@ OR Access_token.given between '2019-08-11'::date and '2019-09-07';
 -- автор проводить одноденну акцiю 50% знижок;                                         X - not specific to author A and customer C
 -- автор проводить тижневу акцiю 50% знижок на повiдомленя деякого стилю;              X - not specific to author A and customer C
 
-PREPARE getEvents (int, int, date, date) AS
+PREPARE getEvents (VARCHAR(50), VARCHAR(50), date, date) AS
 SELECT
 (
-	SELECT CONCAT(post_id) FROM Post
-	WHERE customer_id = $1 AND author_id = $2
+SELECT
+	post_id FROM Post
+	INNER JOIN (SELECT customer_id, name FROM Customer WHERE Customer.name=$1) as C
+	ON Post.customer_id=C.customer_id
+	INNER JOIN (SELECT author_id, name FROM Author WHERE Author.name=$2) AS A
+	ON Post.author_id=A.author_id
 	AND date between
 	$3::date AND $4::date
-) as posts_ids,
+) as posts,
 (
 SELECT 
-	CONCAT(token_id) FROM Access_token
-	WHERE customer_id = $1 AND author_id = $2
+	token_id FROM (Access_token
+	INNER JOIN Customer ON Customer.customer_id=Access_token.customer_id)
+	INNER JOIN Author ON Author.author_id=Access_token.author_id
+	WHERE Customer.name = $1 AND Author.name = $2
 	AND given > $3::date
 ) as tokens_given,
 (
 SELECT 
-	CONCAT(token_id) FROM Access_token
-	WHERE customer_id = $1 AND author_id = $2
+	token_id FROM Access_token
+	INNER JOIN Customer ON Customer.customer_id=Access_token.customer_id
+	INNER JOIN Author ON Author.author_id=Access_token.author_id
+	WHERE Customer.name = $1 AND Author.name = $2
 	AND NOT status AND terminated < $4::date
 ) as tokens_terminated;
 
-EXECUTE getEvents(10, 7, '2000-05-09', '2021-09-10');
+EXECUTE getEvents('Anya', 'Linda', '2000-05-09', '2021-09-10');
 deallocate getEvents;
 
 -- 10.	для покупця С та кожного стилю, у якому вiн замовляв повiдомлення, знайти скiльки замовлень за вказаний перiод
 -- (з дати F по дату T) отримали 50% знижку;
 
-PREPARE getDiscountedPosts (int, date, date) AS
-
+PREPARE getDiscountedPosts (VARCHAR(50), date, date) AS
 SELECT W.style_id as style_id, W.count+D.count as count FROM
 (SELECT style_id, count(style_id)
 			FROM Post INNER JOIN PromosWeek
 			ON Post.date between PromosWeek.start_date and PromosWeek.start_date + '7 days'::interval
-			WHERE customer_id = $1 AND
+			INNER JOIN (SELECT customer_id FROM Customer WHERE Customer.name=$1) as C
+ 			ON C.customer_id = Post.customer_id AND
 			date between $2 and $3
 			GROUP BY style_id) as W
 JOIN
 (SELECT style_id, count(style_id)
 			FROM Post INNER JOIN PromosWeek
 			ON Post.date = PromosWeek.start_date
-			WHERE customer_id = $1 AND
+			INNER JOIN (SELECT customer_id FROM Customer WHERE Customer.name=$1) as C
+ 			ON C.customer_id = Post.customer_id AND
 			date between $2 and $3
 			GROUP BY style_id) as D
 ON W.style_id=D.style_id;
-EXECUTE getDiscountedPosts(1, '2000-05-09', '2021-09-10');
+EXECUTE getDiscountedPosts('Oleksandr', '2000-05-09', '2021-09-10');
 deallocate getDiscountedPosts;
 
 --11
